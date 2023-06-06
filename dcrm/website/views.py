4851cf69c2601253ca760a7c43dc11076a132b56
Michel <omichelsodre@gmail.com>
2023-06-06 14:51:46 -0300

    Logout

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -25,4 +25,6 @@ def login_user(request):
     pass
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request,"You have been logged out.")
+    return redirect('home')
