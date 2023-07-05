77d0fc007c7b192c69289139fb6154271519bfab
Michel <omichelsodre@gmail.com>
2023-07-05 17:37:08 -0300

    Add Record

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 # Create your views here.
@@ -66,3 +66,17 @@ def delete_record(request, pk):
     else:
         messages.success(request,'You Have Be Logged in To Delete!')
         return redirect('home')
+    
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request,'Record Added.')
+                return redirect('home')
+        return render(request,'add_record.html',{'form':form})
+    else:
+        messages.success(request,'You must Be logged in.')
+        return redirect('home')
+
